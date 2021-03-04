7d91aa2e0ffa066784fdcf9c504bb9ce4c8a8e15
Ella van Durpe <ella@vandurpe.com>
2021-03-05 01:13:06 +0200

    Attempt to deprecate useBlockEditContext in favour of useBlockClientId

--- packages/block-editor/src/components/block-edit/index.js
+++ packages/block-editor/src/components/block-edit/index.js
@@ -1,15 +1,67 @@
 /**
+ * WordPress dependencies
+ */
+import { useSelect } from '@wordpress/data';
+import { createContext, useContext } from '@wordpress/element';
+import deprecated from '@wordpress/deprecated';
+
+/**
+ * Internal dependencies
+ */
+import { store as blockEditorStore } from '../../store';
+
+const BlockClientId = createContext();
+
+/**
  * Internal dependencies
  */
 import Edit from './edit';
-import { BlockEditContextProvider, useBlockEditContext } from './context';
 
-export { useBlockEditContext };
+/**
+ * Returns the current (contextual) block client ID.
+ *
+ * @return {string} The block client ID.
+ */
+export function useBlockClientId() {
+	return useContext( BlockClientId );
+}
+
+/**
+ * A hook that returns the block client ID, name and selected status.
+ *
+ * @deprecated
+ *
+ * @return {Object} Block client ID, name and selected status.
+ */
+export function useBlockEditContext() {
+	deprecated( 'wp.blockEditor.useBlockEditContext', {
+		alternative: 'wp.blockEditor.useBlockClientId and wp.data.useSelect',
+	} );
+	const clientId = useBlockClientId();
+	return useSelect(
+		( select ) => {
+			if ( ! clientId ) {
+				return {};
+			}
+
+			const { getBlockName, isBlockSelected } = select(
+				blockEditorStore
+			);
+
+			return {
+				clientId,
+				name: getBlockName( clientId ),
+				isSelected: isBlockSelected( clientId ),
+			};
+		},
+		[ clientId ]
+	);
+}
 
 export default function BlockEdit( props ) {
 	return (
-		<BlockEditContextProvider value={ props.clientId }>
+		<BlockClientId.Provider value={ props.clientId }>
 			<Edit { ...props } />
-		</BlockEditContextProvider>
+		</BlockClientId.Provider>
 	);
 }
